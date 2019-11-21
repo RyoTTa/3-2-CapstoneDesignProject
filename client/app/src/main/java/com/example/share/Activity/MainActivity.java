67e8a7d18488e735aa14db5eade6a6af304c9b7b
Jihye <coco__1@naver.com>
2019-11-21 21:42:22 +0900

    merged by jihye

--- client/app/src/main/java/com/example/share/Activity/MainActivity.java
+++ client/app/src/main/java/com/example/share/Activity/MainActivity.java
@@ -18,27 +18,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-/*
-package com.example.share;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(getApplicationContext(),ReservationInfoActivity.class);
-        intent.putExtra("string","kim_jihye");
-        startActivity(intent);
-
-    }
-}
-
- */
