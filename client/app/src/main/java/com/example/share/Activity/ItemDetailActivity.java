d0bbdb68d55bab0435a2d51a5eae539bb677bc8d
Jihye <coco__1@naver.com>
2019-11-22 17:15:02 +0900

    bucketlist a little

--- client/app/src/main/java/com/example/share/Activity/ItemDetailActivity.java
+++ client/app/src/main/java/com/example/share/Activity/ItemDetailActivity.java
@@ -56,6 +56,8 @@ public class ItemDetailActivity extends AppCompatActivity {
     String item_id;
     ImageView question;
     ImageView reservation;
+    ImageView bucketheart;
+    int i =0;
 
     //mongoDB
     private String MongoDB_IP = "15.164.51.129";
@@ -88,7 +90,7 @@ public class ItemDetailActivity extends AppCompatActivity {
         TextView item_price_per_day = (TextView)findViewById(R.id.item_detail_item_price);
         question = (ImageView)findViewById(R.id.btn_question);
         reservation = (ImageView)findViewById(R.id.btn_reservation);
-
+        bucketheart = (ImageView)findViewById(R.id.bucketheart);
         //item detail, TODO
         TextView item_detail = (TextView)findViewById(R.id.item_detail_item_detail);
         TextView item_location = (TextView)findViewById(R.id.item_detail_item_location);
@@ -191,6 +193,22 @@ public class ItemDetailActivity extends AppCompatActivity {
             deleteBtn.setVisibility(View.INVISIBLE);
         }
 
+        bucketheart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = 1-i;
+
+                if(i==0){
+                    bucketheart.setImageResource(R.drawable.heart);
+                    send_bucketheartdelete(user_email, item_id);
+                }else
+                {
+                    bucketheart.setImageResource(R.drawable.bucketheart2);
+                    send_bucketheartadd(user_email,item_id);
+                }
+
+            }
+        });
 
         return;
     }
@@ -328,5 +346,178 @@ public class ItemDetailActivity extends AppCompatActivity {
                 alert.show();
             }
         }
+    }
+
+    public void send_bucketheartadd(String user, String item_id)
+    {
+        String result="false";
+
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+
+        try {
+            //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("item_id",item_id);
+            jsonObject.accumulate("email",user);
+
+            HttpURLConnection con = null;
+
+            BufferedReader reader = null;
+
+
+            try{
+
+                URL url = new URL("http://ec2-15-164-51-129.ap-northeast-2.compute.amazonaws.com:3000/bucket_insert");
+
+                //연결을 함
+                con = (HttpURLConnection) url.openConnection();
+
+                con.setRequestMethod("POST");//POST방식으로 보냄
+                con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+                con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                con.connect();
+
+                //서버로 보내기위해서 스트림 만듬
+                OutputStream outStream = con.getOutputStream();
+                //버퍼를 생성하고 넣음
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                writer.write(jsonObject.toString());
+                writer.flush();
+                writer.close();//버퍼를 받아줌
+
+                //서버로 부터 데이터를 받음
+                InputStream stream = con.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+                result = buffer.toString();
+
+
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    con.disconnect();
+                }
+                try {
+                    if(reader != null){
+                        reader.close();//버퍼를 닫아줌
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("전송 요청",result);
+        if(result.equals("true")) {
+
+        }
+
+
+    }
+
+
+    public void send_bucketheartdelete(String user, String item_id)
+    {
+        String result="false";
+
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+
+        try {
+            //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("item_id",item_id);
+            jsonObject.accumulate("email",user);
+
+            HttpURLConnection con = null;
+
+            BufferedReader reader = null;
+
+
+            try{
+
+                URL url = new URL("http://ec2-15-164-51-129.ap-northeast-2.compute.amazonaws.com:3000/bucket_delete");
+
+                //연결을 함
+                con = (HttpURLConnection) url.openConnection();
+
+                con.setRequestMethod("POST");//POST방식으로 보냄
+                con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+                con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                con.connect();
+
+                //서버로 보내기위해서 스트림 만듬
+                OutputStream outStream = con.getOutputStream();
+                //버퍼를 생성하고 넣음
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                writer.write(jsonObject.toString());
+                writer.flush();
+                writer.close();//버퍼를 받아줌
+
+                //서버로 부터 데이터를 받음
+                InputStream stream = con.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+                result = buffer.toString();
+
+
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(con != null){
+                    con.disconnect();
+                }
+                try {
+                    if(reader != null){
+                        reader.close();//버퍼를 닫아줌
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d("전송 요청",result);
+        if(result.equals("true")) {
+
+        }
+
+
     }
 }
